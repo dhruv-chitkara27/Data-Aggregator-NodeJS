c3a88665189ebb18855c77deb62fa17419604b6f
Dhruv <dhruvchitkara27@gmail.com>
2018-11-22 20:51:31 +0530

    Added choice for json or csv data

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var http = require('http');
 var request = require('request');
 var fs = require('fs');
 var csv = require('csv');
+var url = require('url');
 
-var request_body = undefined;
+var json_request_body = undefined;
+var csv_request_body = undefined;
 var html_content = undefined;
 
 function createHtmlStringFromJson(retrievedData) {
@@ -68,25 +70,31 @@ function createHtmlStringFromCsv(retrievedData) {
 }
 
 
-//request('https://www.bnefoodtrucks.com.au/api/1/trucks', function(err,request_res,body) {
+request('https://www.bnefoodtrucks.com.au/api/1/trucks', function(err,request_res,body) {
 
-  //      request_body = body;
-//});
+        json_request_body = body;
+});
 
 request('https://www.data.brisbane.qld.gov.au/data/dataset/1e11bcdd-fab1-4ec5-b671-396fd1e6dd70/resource/3c972b8e-9340-4b6d-8c7b-2ed988aa3343/download/public-art-open-data-2018-10-22.csv', function(err,request_res,body) {
 
         csv.parse(body, function(err, data){
-          request_body = data;
+          csv_request_body = data;
         });
 });
 
 
 http.createServer(function(req,res){
-  if(request_body && html_content){
+  if(json_request_body && csv_request_body && html_content){
     res.writeHead(200, {'Content-Type': 'text/html'});
-  //  res.end(createHtmlStringFromJson(JSON.parse(request_body)));
-  res.end(createHtmlStringFromCsv(request_body));
-
+    var request_url = url.parse(req.url);
+    switch(request_url.path) {
+      case '/json' :
+      res.end(createHtmlStringFromJson(JSON.parse(json_request_body)));
+      break;
+      case '/csv' :
+      res.end(createHtmlStringFromCsv(csv_request_body));
+      break;
+    }
   } else {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Nothing retrieved yet");
